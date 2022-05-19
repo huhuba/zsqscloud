9e451cf7b3eafdd42f62aa8a5fd2d1633e5cae46
huhuba <438077390@qq.com>
2022-05-19 23:58:23 +0800

    gateway:2022-5-19

---
+++ getway9999/src/main/java/com/getway9999/Getway9999Application.java
@@ -0,0 +1,13 @@
+package com.getway9999;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Getway9999Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Getway9999Application.class, args);
+    }
+
+}
