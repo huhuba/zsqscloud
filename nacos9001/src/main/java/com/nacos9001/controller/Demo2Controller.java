5c02097428ead5aa1fa16522992d4e8b17894e3b
huhuba <438077390@qq.com>
2022-05-25 09:57:16 +0800

    gateway:2022-5-25

---
+++ nacos9001/src/main/java/com/nacos9001/controller/Demo2Controller.java
@@ -0,0 +1,23 @@
+package com.nacos9001.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/zsq")//路由路径
+public class Demo2Controller {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/get")
+    public String getServerPort() {
+        return "库存-1" + serverPort;
+    }
+    @GetMapping(value = "/get1")
+    public  String getServerPort2(){
+        return  "hello  nacos Discovery:"+serverPort;
+    }
+
+}
